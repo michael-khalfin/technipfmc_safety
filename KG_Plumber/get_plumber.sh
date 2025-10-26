8c21f182073b55ae77cb8ca8dfeda10a85ede7ba
Carlos Morales <cdm8@rice.edu>
2025-10-26 17:44:22 -0500

    updated bash file

--- KG_Plumber/get_plumber.sh
+++ KG_Plumber/get_plumber.sh
@@ -2,11 +2,14 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 PLUMBER_DIR="${SCRIPT_DIR}/plumber"
 
 # Plumber Config
-REPO_URL="${PLUMBER_REPO_URL:-https://github.com/orkg/plumber.git}"
-REPO_BRANCH="${PLUMBER_BRANCH:-main}"
+REPO_URL="${PLUMBER_REPO_URL:-https://github.com/YaserJaradeh/ThePlumber}"
+REPO_BRANCH="${PLUMBER_BRANCH:-master}"
 
 echo "Cloning ${REPO_URL} (branch ${REPO_BRANCH})"
-git clone "${REPO_BRANCH}" "${REPO_URL}" "${PLUMBER_DIR}"
+git clone --branch "${REPO_BRANCH}" "${REPO_URL}" "${PLUMBER_DIR}"
+
+# For reference:
+# ./KG_Plumber/get_plumber.sh
 
 
-# Need to Figure out Docker For Now
+# Docker Setup Later (Currently, Plumber works on Local but we are finding ways to utilize it within NOTS)
